9b54de1833cdc749056944eabf93e6cb4ae8c252
LuKeTeSky <lucas.ciaccio@gmail.com>
2021-05-02 09:37:42 -0300

    cambio_2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello groso del mundo mundial!'));
+app.get('/', (req, res) => res.send('Hello groso del mundo mundial del universo SRE!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
